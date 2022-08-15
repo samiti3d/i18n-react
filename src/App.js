daaadfcc039d8c7cb36719e404b964dc121015ed
resmith <36131619+samiti3d@users.noreply.github.com>
2022-08-15 21:37:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import {useTranslation } from "react-i18next"
+// https://www.codeandweb.com/babeledit/tutorials/how-to-translate-your-react-app-with-react-i18next
+function HeaderComponent(){
+  const {t, i18n} = useTranslation("common");
+  return <div>
+    <h1>{t('welcome.title', {framework: 'React'})}</h1>
+    <button onClick={() => i18n.changeLanguage('de')}>de</button>
+    <button onClick={() => i18n.changeLanguage('en')}>en</button>
+  </div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Suspense fallback="loading">
+    <div className='App'>
+      <HeaderComponent />
     </div>
+  </Suspense>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+import common_de from "./translations/de/common.json";
+import common_en from "./translations/en/common.json";
+
+i18next.init({
+  interpolation: { escapeValue: false },  
+  lng: 'en',                              // language to use
+  resources: {
+      en: {
+          common: common_en               // 'common' is our custom namespace
+      },
+      de: {
+          common: common_de
+      },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
